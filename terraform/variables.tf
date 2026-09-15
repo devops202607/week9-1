aa4ccadc0035a9fab3a83a83b0b2a622f88554ee
derifgig <dima@wdm.net.ua>
2026-09-16 00:05:40 +0300

    fix: correct tempo otlp receivers

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -24,7 +24,7 @@ variable "node_count" {
 variable "machine_type" {
   description = "GCE machine type for nodes"
   type        = string
-  default     = "e2-small"
+  default     = "e2-standard-2"
 }
 
 variable "disk_size_gb" {
